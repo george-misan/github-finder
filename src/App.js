ed767fe23a0ad8718d1f7f3edbf2974371cf19ee
iamgeorge <george.eyoyibo@gmail.com>
2020-03-16 01:44:27 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <nav className="navbar bg-primary">
+        <Navbar />
+      </nav>
+    );
+  }
 }
 
 export default App;
+
+/* class App extends Component {
+  moo = () => 'Harley Quinn';
+  render() {
+    const name = 'Helsinki';
+    const loading = false;
+    const showName = true;
+
+    const foo = a => a + 22; 
+    return (
+      <div className="App">
+        {loading ? (
+          <h1>Loading....</h1>
+        ) : (
+          <h1>Hello from {showName && name}</h1>
+        )}
+        <h2>Good day {this.moo()}</h2>
+      </div>
+    );
+  }
+}
+
+export default App; */
