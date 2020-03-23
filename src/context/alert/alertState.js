3bcdd2fd31141bfdefe7d3ed16418628f5d279de
iamgeorge <george.eyoyibo@gmail.com>
2020-03-23 22:22:18 +0200

    ready for deployment

--- src/context/alert/alertState.js
+++ src/context/alert/alertState.js
@@ -1,5 +1,4 @@
 import React, { useReducer } from 'react';
-import axios from 'axios';
 import AlertContext from './alertContext';
 import alertReducer from './alertReducer';
 import { SET_ALERT, REMOVE_ALERT } from '../types';
