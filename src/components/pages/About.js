774518e1201978af70bab695cfa9bf7e2f51a8cb
iamgeorge <george.eyoyibo@gmail.com>
2020-04-09 02:26:21 +0300

    testing CI

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,9 @@ import React, { Fragment } from 'react';
 const About = () => {
   return (
     <Fragment>
-      <h1>About this App</h1>
+      <h1>About this Application</h1>
       <p>App to search Github users</p>
-      <p>Version: 1.0.0</p>
+      <p>Version: 1.0.2</p>
     </Fragment>
   );
 };
