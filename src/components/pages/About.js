15bd41338a0e572f187a3a99d9c2d82112292e2e
iamgeorge <george.eyoyibo@gmail.com>
2020-04-09 03:07:40 +0300

    fix typo

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,9 @@ import React, { Fragment } from 'react';
 const About = () => {
   return (
     <Fragment>
-      <h1>About this Application</h1>
+      <h1>About this App</h1>
       <p>App to search Github users</p>
-      <p>Version: 1.0.2</p>
+      <p>Version: 1.0.1</p>
     </Fragment>
   );
 };
